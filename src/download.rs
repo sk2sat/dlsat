74c79e32e35ddb3a8bf6154ca8c3462dd7abdf85
sksat <sksat@sksat.net>
2021-01-07 02:17:47 +0900

    add: fuck python

--- src/download.rs
+++ src/download.rs
@@ -1,3 +1,6 @@
+use futures::executor::ThreadPool;
+use inline_python::python;
+use std::sync::{Arc, Mutex};
 use url::{Host::Domain, Url};
 use youtube_dl::{YoutubeDl, YoutubeDlOutput};
 
@@ -67,6 +70,8 @@ impl Target {
             match info {
                 YoutubeDlOutput::SingleVideo(sv) => {
                     log::info!("downloading single video: {}", sv.title);
+                    let url = &self.s;
+                    do_youtube_dl(url);
                 }
                 YoutubeDlOutput::Playlist(pl) => {
                     log::info!("downloading playlist...");
@@ -74,4 +79,88 @@ impl Target {
             }
         }
     }
+}
+
+use inline_python::pyo3::{
+    prelude::*, pyclass_slots::PyClassDictSlot, types::IntoPyDict, types::PyDict, wrap_pyfunction,
+    FromPyObject, Py, PyClass, PyObject, PyResult, Python,
+};
+
+//use pyo3::prelude::*;
+#[pyclass(dict)]
+//#[derive(Clone)]
+struct YtStatus {
+    status: String,
+    downloaded_bytes: usize,
+    fragment_index: usize,
+    fragment_count: usize,
+    filename: String,
+    tmpfilename: String,
+    elapsed: f64,
+    total_bytes_estimate: f64,
+    eta: usize,
+    speed: f64,
+    _eta_str: String,
+    _percent_str: String,
+    _speed_str: String,
+    _total_bytes_estimate_str: String,
+}
+
+//use pyo3::prelude::*;
+//use pyo3::wrap_pyfunction;
+#[pyfunction]
+fn phook_rs(py: pyo3::Python, d: &PyDict) {
+    println!("hook");
+    //let d: &YtStatus = d.into();
+}
+
+fn do_youtube_dl(url: &str) {
+    //let mut percent = String::new();
+    let ctx: inline_python::Context = python! {
+        status = {}
+        downloaded = 0
+    };
+    let ctx = Arc::new(ctx);
+    //let percent0 = Arc::new(percent);
+
+    {
+        // progress monitor thread
+        let pool = ThreadPool::new().unwrap();
+        let ctx = ctx.clone();
+        //let p = percent0.clone();
+        pool.spawn_ok(async move {
+            loop {
+                //log::info!("{}", p);
+                //log::info!("{}", ctx.get::<i32>("downloaded"));
+                //log::info!("{}", ctx.get::<String>("percent"));
+                //let gil = Python::acquire_gil();
+                //let py = gil.python();
+                //let status: PyObject = ctx.get::<PyObject>("status").extract(py).unwrap();
+                //let status: PyDict = status.extract(py).unwrap();
+                //let s = ctx.get::<YtStatus>("status").into();
+                //log::info!("{}", s);
+                std::thread::sleep(std::time::Duration::from_millis(50));
+            }
+        });
+    }
+
+    ctx.add_wrapped(wrap_pyfunction!(phook_rs));
+
+    ctx.run(python! {
+        import youtube_dl
+        import time
+
+        def phook(d):
+            global status
+            global downloaded
+            status = d
+            downloaded = d["downloaded_bytes"]
+            //'percent = d["_percent_str"]
+            #print(time.time())
+
+        ret = youtube_dl.YoutubeDL(params={
+            "quiet": True,
+            "progress_hooks": [lambda d: phook_rs(d)],
+        }).download(['url])
+    });
 }
